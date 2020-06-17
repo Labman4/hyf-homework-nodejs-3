aedb0ad270391ba15c0e0a825f38c4cfa1851da9
Labman4 <50023562+Labman4@users.noreply.github.com>
2020-06-17 11:39:43 +0800

    Hello World!

--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ const express=require("express");
 const port=3000;
 const app=express();
 app.get("/",(req,res)=>{
-	res.json(
-	{
-		content:"Hello World"
-	});
+	res.send("Hello World!");
 })
 app.listen(port,()=>{
-	console.log("Hello World");
+	console.log("1");
 })
